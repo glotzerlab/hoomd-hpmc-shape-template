87a4c4b95bdb7cf3ea2226724dacd32504420a19
Joshua A. Anderson <joaander@umich.edu>
2024-12-02 12:06:45 -0500

    Port shape_plugin from HOOMD-blue.

---
+++ src/integrate.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2009-2024 The Regents of the University of Michigan.
+# Part of HOOMD-blue, released under the BSD 3-Clause License.
+
+"""Example Shape Integrator."""
+
+# Import the C++ module
+from . import _template
+
+import hoomd
+
+
+class MySphere(hoomd.hpmc.integrate.HPMCIntegrator):
+    """Example shape integrator."""
+
+    # set static class data
+    _ext_module = _template
+    _cpp_cls = "IntegratorHPMCMonoMySphere"
+
+    def __init__(
+        self,
+        default_d=0.1,
+        default_a=0.1,
+        translation_move_probability=0.5,
+        nselect=4,
+        kT=1.0,
+    ):
+        # initialize base class
+        super().__init__(
+            default_d, default_a, translation_move_probability, nselect, kT
+        )
+
+        typeparam_shape = hoomd.data.typeparam.TypeParameter(
+            "shape",
+            type_kind="particle_types",
+            param_dict=hoomd.data.parameterdicts.TypeParameterDict(
+                radius=float, ignore_statistics=False, orientable=False, len_keys=1
+            ),
+        )
+        self._add_typeparam(typeparam_shape)
+
+    @hoomd.logging.log(category="object", requires_run=True)
+    def type_shapes(self):
+        """list[dict]: Description of shapes in ``type_shapes`` format."""
+        return super()._return_type_shapes()
--- src/module.cc
+++ src/module.cc
@@ -4,10 +4,32 @@
 // TODO: Include the header files of classes that will be exported to Python.
 
 #include <pybind11/pybind11.h>
+#include "hoomd/hpmc/ComputeFreeVolume.h"
+#include "hoomd/hpmc/IntegratorHPMC.h"
+#include "hoomd/hpmc/IntegratorHPMCMono.h"
+
+#include "hoomd/hpmc/ComputeSDF.h"
+#include "hoomd/hpmc/ShapeUnion.h"
+
+#include "hoomd/hpmc/ExternalFieldHarmonic.h"
+#include "hoomd/hpmc/ExternalFieldWall.h"
+
+#include "hoomd/hpmc/UpdaterGCA.h"
+#include "hoomd/hpmc/UpdaterMuVT.h"
+
+#include "ShapeMySphere.h"
+
+#ifdef ENABLE_HIP
+#include "hoomd/hpmc/ComputeFreeVolumeGPU.h"
+#include "hoomd/hpmc/IntegratorHPMCMonoGPU.h"
+#include "hoomd/hpmc/UpdaterGCAGPU.h"
+#endif
+
+using namespace hoomd::hpmc::detail;
 
 namespace hoomd
     {
-namespace md
+namespace hpmc
     {
 
 // TODO: Set the name of the python module to match ${COMPONENT_NAME} (set in
@@ -15,10 +37,24 @@ namespace md
 PYBIND11_MODULE(_template, m)
     {
         // TODO: Call export_Class(m) for each C++ class to be exported to Python.
+    export_IntegratorHPMCMono<ShapeMySphere>(m, "IntegratorHPMCMonoMySphere");
+    export_ComputeFreeVolume<ShapeMySphere>(m, "ComputeFreeVolumeMySphere");
+    export_ComputeSDF<ShapeMySphere>(m, "ComputeSDFMySphere");
+    export_UpdaterMuVT<ShapeMySphere>(m, "UpdaterMuVTMySphere");
+    export_UpdaterGCA<ShapeMySphere>(m, "UpdaterGCAMySphere");
+
+    export_ExternalFieldWall<ShapeMySphere>(m, "WallMySphere");
+
+    pybind11::class_<MySphereParams, std::shared_ptr<MySphereParams>>(m, "MySphereParams")
+        .def(pybind11::init<pybind11::dict>())
+        .def("asDict", &MySphereParams::asDict);
 
 #ifdef ENABLE_HIP
         // TODO: Call export_ClassGPU(m) for each GPU enabled C++ class to be exported
         // to Python.
+    export_IntegratorHPMCMonoGPU<ShapeMySphere>(m, "IntegratorHPMCMonoMySphereGPU");
+    export_ComputeFreeVolumeGPU<ShapeMySphere>(m, "ComputeFreeVolumeMySphereGPU");
+    export_UpdaterGCAGPU<ShapeMySphere>(m, "UpdaterGCAMySphereGPU");        
 #endif
     }
 
